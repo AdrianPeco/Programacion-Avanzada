edc8b467dc20a394f21f467597a20afff3ec8aaa
Adrian Peco <79875849+AdrianPeco@users.noreply.github.com>
2021-03-18 21:19:22 -0600

    Add files via upload

---
+++ ex_3_2.py
@@ -0,0 +1,11 @@
+c=input("escribe un caracter")
+if ( c=="a" or c=="e" or c=="i" or  c=="u"):
+    print( c ,"es una vocal") 
+else:
+    print("no es la letra c")
+    if ( c=="b" or c=="c" or c=="d" or  c=="f" or c=="g" or c=="h" or c=="j" or c=="k" or c=="l" or c=="m" or c=="n" or c=="ñ" or c=="p" or c=="q" or c=="r" or c=="s" or c=="t" or c=="w" or c=="x" or c=="y" or c=="z"):
+        print(c, "esta en minuscula")
+    else:
+        print(c, "esta en mayuscula")
+        if ( c=="A" or c=="B" or c=="C" or  c=="D" or c=="E" or c=="F" or c=="G" or c=="H" or c=="I" or c=="J" or c=="K" or c=="L" or c=="M" or c=="N" or c=="Ñ" or c=="O" or c=="P" or c=="Q" or c=="R" or c=="S" or c=="T" or c=="U" or c=="V" or c=="W"or c=="X" or c=="Y" or c== "Z"):
+            print(c, "es un simbolo del alfabeto")
